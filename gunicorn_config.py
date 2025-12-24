ff5a4eac42400f86fb0e15fe142ad2de0ff6af30
zhaoyunfeng <zhaoyunfeng@flowgpt.com>
2025-12-24 15:41:47 +0800

    feat: create gunicorn worker gradually

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,11 +1,31 @@
 import os
+import signal
+import threading
+import time
+
 from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
 
 # Gunicorn config variables
-workers = int(os.environ.get('WORKERS', '4'))
+workers = 1
 bind = "0.0.0.0:8080"
 preload_app = True
 
+
+def when_ready(server):
+    remain_workers = int(os.environ.get('WORKERS', '4')) - workers
+    master_pid = os.getpid()
+
+    def create_worker_gradually():
+        for _ in range(remain_workers):
+            time.sleep(5)
+            os.kill(master_pid, signal.SIGTTIN)
+
+    threading.Thread(
+        target=create_worker_gradually,
+        daemon=True,
+    ).start()
+
+
 # For prometheus metrics
 def child_exit(server, worker):
-    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid) 
+    GunicornInternalPrometheusMetrics.mark_process_dead_on_child_exit(worker.pid)
